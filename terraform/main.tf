d3732305482f56e7ea6e784e81bcba598573c9a8
Suchitaa <pradhansuchita06@gmail.com>
2025-07-22 19:26:49 +0530

    Initial commit: DevSecOps project

---
+++ terraform/main.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+  name = "suchita-vpc"
+  cidr = "10.0.0.0/16"
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.10.0"
+  cluster_name    = "suchita-cluster"
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.medium"]
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+  access_entries = {
+    ClusterAdmin = {
+      principal_arn = "arn:aws:iam::520864642809:user/sit-user"
+      policy_associations = {
+        Admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy",
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+    MyDevOpsUser = {
+      principal_arn = "arn:aws:iam::176387410897:user/project-devops"
+      policy_associations = {
+        Admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy",
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
